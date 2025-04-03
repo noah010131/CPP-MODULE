7ae9c577f5521f1d7b49301c8c5528ef53e8100c
chanypar <noah010131#gmail.com>
2025-04-03 16:46:15 +0200

    cpp05

---
+++ cpp05/ex00/Bureaucrat.cpp
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chanypar <chanypar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/03 16:09:06 by chanypar          #+#    #+#             */
+/*   Updated: 2025/04/03 16:40:12 by chanypar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+const char * Bureaucrat::GradeTooHighException::what(void) const throw()
+{
+	return "Grade too high...";
+}
+
+const char * Bureaucrat::GradeTooLowException::what(void) const throw()
+{
+	return "Grade too low...";
+}
+
+Bureaucrat::Bureaucrat() : _name("default"), _grade(150)
+{
+
+}
+
+Bureaucrat::Bureaucrat(const std::string name, int grade) : _name(name)
+{	
+	if (grade < 1)
+		throw GradeTooHighException();
+	else if (grade > 150)
+		throw GradeTooLowException();
+	else
+		_grade = grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other) : _name(other._name), _grade(other._grade)
+{
+
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other)
+{
+	if (this != &other)
+		_grade = other._grade;	
+	return (*this);
+}
+
+
+Bureaucrat::~Bureaucrat()
+{
+
+}
+
+// Member functions
+std::string Bureaucrat::getName() const
+{
+	return (_name);
+}
+
+int Bureaucrat::getGrade() const
+{
+	return (_grade);
+}
+
+void Bureaucrat::incrementGrade()
+{
+	if (_grade == 1)
+		throw GradeTooHighException();
+	else
+	_grade--;
+}
+
+void Bureaucrat::decrementGrade()
+{
+	if (_grade == 150)
+		throw GradeTooLowException();
+	else
+		_grade++;
+}
+
+std::ostream &operator<<(std::ostream &os, Bureaucrat const &other)
+{
+	os << other.getName() << ", bureaucrat grade " << other.getGrade() << std::endl;
+	return (os);
+}
+
---
+++ cpp05/ex00/main.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chanypar <chanypar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/03 16:31:59 by chanypar          #+#    #+#             */
+/*   Updated: 2025/04/03 16:42:54 by chanypar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int main(void)
+{
+	try
+	{
+		try
+		{
+			Bureaucrat test("test", 200);	
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		
+		Bureaucrat a("a", 150);
+		Bureaucrat b("b", 1);
+
+		std::cout << a << std::endl;
+		std::cout << b << std::endl;
+		std::cout << std::endl;
+		
+		// a.decrementGrade();
+		// b.incrementGrade();
+		// std::cout << a << std::endl;
+		// std::cout << b << std::endl;
+		// std::cout << std::endl;
+
+		a.incrementGrade();
+		b.decrementGrade();
+		std::cout << a << std::endl;
+		std::cout << b << std::endl;
+		std::cout << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	return (0);
+}
