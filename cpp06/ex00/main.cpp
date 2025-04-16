29c5967938ff3a66daecd436729b3116ac3bea87
chanypar <noah010131#gmail.com>
2025-04-16 11:09:48 +0200

    cpp06

---
+++ cpp06/ex00/main.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chanypar <chanypar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/16 10:43:56 by chanypar          #+#    #+#             */
+/*   Updated: 2025/04/16 11:08:25 by chanypar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Conversion.hpp"
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cerr << "One Argument required." << std::endl;
+		return (1);
+	}
+	try
+	{
+		Conversion conversion(argv[1]);
+		// try
+		// {
+		// 	Conversion copy(conversion);
+		// }
+		// catch(const std::exception& e)
+		// {
+		// 	std::cerr << e.what() << std::endl;
+		// }
+	}
+	catch(const Conversion::ErrorException& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	return (0);
+}
